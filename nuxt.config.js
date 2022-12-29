5ad4a9c61932cdea8eeea68c056f2d9792a4adf3
jaroslaw-filipiak <info@j-filipiak.pl>
2022-12-29 17:58:40 +0100

    og img

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,27 @@ export default {
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: '' },
+            { title: 'Projektowanie stron www -  tylko profesjonalne strony firmowe' },
+            {
+                hid: 'description',
+                name: 'description',
+                content:
+                    'Profesjonalne strony www. 10 lat doświadczenia. Nowoczesne strony zoptymalizowane pod Google. Optymalizacja UX. Bezpłatna Wycena - Poproś o indywidualną wycenę!. Szybki kontakt',
+            },
+
+            { hid: 'og-type', property: 'og:type', content: 'website' },
+            {
+                hid: 'og-title',
+                property: 'og:title',
+                content: 'Projektowanie stron www -  tylko profesjonalne strony firmowe',
+            },
+            {
+                hid: 'og-desc',
+                property: 'og:description',
+                content:
+                    'Profesjonalne strony www. 10 lat doświadczenia. Nowoczesne strony zoptymalizowane pod Google. Optymalizacja UX. Bezpłatna Wycena - Poproś o indywidualną wycenę!. Szybki kontakt',
+            },
+            { hid: 'og-image', property: 'og:image', content: 'https://j-filipiak.pl/og-img.jpg' },
         ],
         link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     },
